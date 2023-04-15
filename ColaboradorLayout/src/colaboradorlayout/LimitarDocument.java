133aea5f82b00c02301db20400282e078a104968
Caio-eng-gif <caioc.1997@hotmail.com>
2023-04-15 18:38:23 -0300

    atualização 2023.04.15

---
+++ ColaboradorLayout/src/colaboradorlayout/LimitarDocument.java
@@ -0,0 +1,26 @@
+package colaboradorlayout;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+public class LimitarDocument extends PlainDocument {
+    private int limiteCaracteres;
+
+    public LimitarDocument(int limiteCaracteres) {
+        this.limiteCaracteres = limiteCaracteres;
+    }
+
+    @Override
+    public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+        if (str == null) {
+            return;
+        }
+
+        if ((getLength() + str.length()) <= limiteCaracteres) {
+            super.insertString(offset, str, attr);
+        }
+    }
+}
+
---
+++ ColaboradorLayout/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module ColaboradorLayout {
+	requires java.desktop;
+}
